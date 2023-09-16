6e724493764cac468913cf5e7b333068a0240d1d
EgorPestov <egorpestov2012@yandex.ru>
2023-09-16 16:08:58 +0400

    fix: fixes the problem with linter (more info inside)

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -13,6 +13,11 @@ module.exports = {
   settings: { react: { version: 'detect' } },
   plugins: ['react-refresh'],
   rules: {
+    "@typescript-eslint/no-misused-promises": [2, {
+      "checksVoidReturn": {
+        "attributes": false
+      }
+    }], // см. https://github.com/orgs/react-hook-form/discussions/8622 и ответ к ней
     'react-refresh/only-export-components': 'warn',
   },
   overrides: [
